3ccc06d8362b66e53f32ffb07cd2324863aa6690
Pedro Nunes <pedro.nunes@techdog.com.br>
2025-09-02 14:53:53 -0300

    feat: lógica de renderização dinâmica de página

--- js/fetch.js
+++ js/fetch.js
@@ -1,13 +1,40 @@
-const loadPage = async (page) => {
-  try {
-    const response = await fetch(page);
-    const html = await response.text();
-    document.getElementById("content").innerHTML = html;
-  } catch (error) {
-    console.error("Erro ao carregar a página", error);
-  }
-};
+const iframe = document.getElementById("content-frame");
 
-document.addEventListener("DOMContentLoaded", () => {
-  loadPage("./pages/home/home.html");
+function injetarCSS(doc, page) {
+  const linkEl = doc.createElement("link");
+  linkEl.rel = "stylesheet";
+  linkEl.type = "text/css";
+
+  if (page.includes("teste.html")) {
+    linkEl.href = "/css/teste/teste.css";
+  }
+
+  if (page.includes("home.html")) {
+    linkEl.href = "/css/home/home.css";
+  }
+
+  doc.head.appendChild(linkEl);
+}
+
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const page = link.getAttribute("data-page");
+    console.log(page);
+
+    iframe.src = page;
+
+    iframe.onload = function () {
+      const doc = iframe.contentDocument || iframe.contentWindow.document;
+      injetarCSS(doc, page);
+    };
+  });
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  iframe.onload = function () {
+    const doc = iframe.contentDocument || iframe.contentWindow.document;
+    const page = iframe.getAttribute("src");
+    injetarCSS(doc, page);
+  };
 });
